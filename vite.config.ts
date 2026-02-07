a5698bcbacc3a9a77b6a3c5d100a85bacf516193
takurou19980823@yahoo.co.jp <takurou19980823@yahoo.co.jp>
2026-02-07 17:01:02 +0900

    デプロイ設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES // この行を追加
+    ? "header-footer-test" // この行を追加
+    : "./",
   plugins: [react()],
-})
+});
